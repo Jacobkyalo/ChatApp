6c42e97a225142943b8a6ee158b46123bf9d9393
Jacob <jacobkyalo43@gmail.com>
2022-03-27 22:14:45 +0300

    added app.js for functinality

---
+++ app.js
@@ -0,0 +1,27 @@
+const icons = document.querySelector('#icons');
+const call = document.querySelector('#call');
+const archive = document.querySelector('#archive');
+const icons2 = document.querySelector('#icons2');
+const contactPerson = document.querySelector('.contact-person');
+
+
+icons.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+})
+archive.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+})
+call.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+})
+icons2.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+})
+contactPerson.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+})
