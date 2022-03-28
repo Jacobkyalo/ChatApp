7698dc31ea5c93de9783b98a7b950bad78e9f351
Jacob <jacobkyalo43@gmail.com>
2022-03-28 17:36:27 +0300

    add alert functionality to the icons

--- app.js
+++ app.js
@@ -1,27 +1,64 @@
-const icons = document.querySelector('#icons');
+//making the icons to display no functionality
+
+const home = document.querySelector('#home');
 const call = document.querySelector('#call');
 const archive = document.querySelector('#archive');
-const icons2 = document.querySelector('#icons2');
+const chaticon1 = document.querySelector('#chat-icon1');
+const chaticon2 = document.querySelector('#chat-icon2');
+const chaticon3 = document.querySelector('#chat-icon3');
+const chaticon4 = document.querySelector('#chat-icon4');
+const chaticon5 = document.querySelector('#chat-icon5');
+const chaticon6 = document.querySelector('#chat-icon6');
 const contactPerson = document.querySelector('.contact-person');
 
+//adding event listeners
 
-icons.addEventListener('click', (e) => {
+home.addEventListener('click', (e) => {
   e.preventDefault();
  alert('No functionality');
-})
-archive.addEventListener('click', (e) => {
-  e.preventDefault();
- alert('No functionality');
-})
+});
+
 call.addEventListener('click', (e) => {
   e.preventDefault();
  alert('No functionality');
-})
-icons2.addEventListener('click', (e) => {
+});
+
+archive.addEventListener('click', (e) => {
   e.preventDefault();
  alert('No functionality');
-})
+});
+
+chaticon1.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+});
+
+chaticon2.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+});
+
+chaticon3.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+});
+
+chaticon4.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+});
+
+chaticon5.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+});
+
+chaticon6.addEventListener('click', (e) => {
+  e.preventDefault();
+ alert('No functionality');
+});
+
 contactPerson.addEventListener('click', (e) => {
   e.preventDefault();
  alert('No functionality');
-})
+});
